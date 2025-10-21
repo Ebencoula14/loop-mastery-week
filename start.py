b0bc9f1e2386fee499810c118d4442f0e2a2d3ab
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-21 22:31:18 +0100

    introduction to LOOPs continued

--- start.py
+++ start.py
@@ -1,8 +1,5 @@
-number_list = [5, 3, 8, 6, 2, 10]
-new_number_list = []
-
-for i in number_list:
+number = 0
+for i in range(1,101):
     if i % 2 == 0:
-        i = i ** 2
-    new_number_list.append(i)
-print(new_number_list)
+        number +=i
+print("The sum of alL EVEN numbers is: ", number)
