046afd092d3316f762e2942920da26a86257a6a8
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-21 20:43:59 +0100

    introduction to LOOPs

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
-for i in range(1, 6):
-    for j in range(1, i + 1):
-        print("*", end= " ")
-    print()
+number = 0
+odd = 0
+for i in range(1,101):
+    if i % 2 == 0:
+        number += i
+    else:
+        odd += i
+print(f"The sum of EVEN numbers is {number}. and The sum of all ODD numbers is {odd}")
