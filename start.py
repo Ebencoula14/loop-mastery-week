0cb2bddd8074685b43144fb87497e13af2b130db
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-23 12:24:31 +0100

    introduction to LOOPs continued

--- start.py
+++ start.py
@@ -1,2 +1,5 @@
-for i in range(1,51):
-    print("*" * i)
+n =int(input("kindly pick any random number?"))
+number  = 1
+for i in range(1,  n+ 1):
+    number *= i
+print("The factorial  of the number you picked : ", number)
