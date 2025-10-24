1f0f77203b1036e7b4c591f5ffabf31d4284dc8a
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-24 23:38:41 +0100

    introduction to LOOPs continued_number of ODD numbers  BUT not divisble by 6 between 1 and 200

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 number = []
-for i in range(1, 101):
-    if i % 2 != 0 and i % 5 == 0:
+for i in range(1, 201):
+    if i % 3 == 0 and not i % 6 == 0:
         number.append(i)
 print(len(number))
 
