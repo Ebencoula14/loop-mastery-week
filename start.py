09efd4629568f5e94dd413e6bf8102de7bfdf988
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-25 22:37:50 +0100

    introduction to LOOPs continued.

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
-number = []
-for i in range(1, 201):
-    if i % 3 == 0 and not i % 6 == 0:
-        number.append(i)
-print(len(number))
-
+number = 0
+for i in range(1, 301):
+    if (i % 4 == 0 or i % 9 == 0) and not ( i % 4 == 0 and i % 9 == 0):
+        number += 1
+print(number)
