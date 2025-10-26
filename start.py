26380ac5dc19f820ddea1e4f08e4b87ee0c4c521
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-26 14:43:45 +0100

    introduction to LOOPs continued.

--- start.py
+++ start.py
@@ -2,4 +2,4 @@ number = 0
 for i in range(1, 301):
     if (i % 4 == 0 or i % 9 == 0) and not ( i % 4 == 0 and i % 9 == 0):
         number += 1
-print(number)
+print("The total number of numbers is : ", number)
