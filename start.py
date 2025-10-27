26581234f4b351c956b15a608673313d45cccaa8
Ebencoula14 <Ebencoula14@gmail.com>
2025-10-27 13:28:55 +0100

    introduction to LOOPs continued.

--- start.py
+++ start.py
@@ -1,7 +1,5 @@
-n = int(input("Kindly choose any random number bro?"))
-for i in range(1, n + 1):
-    if i % 2 == 0:
-        i = "*" * i
-    else:
-        i = str(i) * i
-    print(i)
+name = "python"
+if name== "java" or "html":
+    print("login valid")
+else:
+    print("nott valid")
