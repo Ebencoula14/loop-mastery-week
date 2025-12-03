6d9b7ab0ceaa370582c0e4788f40a5adb48f262d
Ebencoula14 <Ebencoula14@gmail.com>
2025-12-03 23:14:03 +0100

    introduction to loops

--- start.py
+++ start.py
@@ -1,4 +1,9 @@
 scores = 0
+age = 0
 for number in range(0, 101):
-    scores += number
-print("The sum of numbers is : ", scores)
+    if number % 2 == 0:
+        scores += number
+    else:
+        age +=  number
+
+print(f"The sum of EVEN number is {scores}. And the sum of ODD number is {age}.")
