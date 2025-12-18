8176ea160e260f84b976f7ffc6e5c206793945b6
Ebencoula14 <Ebencoula14@gmail.com>
2025-12-18 18:31:17 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
 count = 0
+score = 0
 for i in range(0, 101):
-    count += i
-print("The sum of numbers from 1 to 100 is: ", count)
+    if i % 2 == 0:
+        count += i
+    else:
+        score += i
+print(f"The sum of EVEN is {count} while the sum of ODD numbers is {score}")
