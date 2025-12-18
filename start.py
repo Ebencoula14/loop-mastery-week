0f3f625aa3f83062725405f2d39cb21581f1fad0
Ebencoula14 <Ebencoula14@gmail.com>
2025-12-18 18:38:37 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,8 +1,3 @@
-count = 0
-score = 0
-for i in range(0, 101):
-    if i % 2 == 0:
-        count += i
-    else:
-        score += i
-print(f"The sum of EVEN is {count} while the sum of ODD numbers is {score}")
+fruit = ['banana', 'orange', 'mango', 'lemon']
+for i in reversed(fruit):
+    print(i)
