a286c98ea240291ac1bd7e27a0828d9bda7e7827
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-01 20:40:10 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-for i in range(1,6):
-    for k in range(i +1):
-        print(k, end=" ")
-    print()
+number = input("Enter a number please\n")
+score = int(number)
+count = 0
+for i in range(score + 1):
+    count += i
+print(f"The SUM of numbers in {score} is : ", count)
