89e3d6e68ee7a348dc620c75ec6706de09597a55
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-02 04:11:26 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,4 +1,3 @@
 my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-for index, value in enumerate(my_list):
-    if index % 2 != 0:
-        print(value, end=" ")
+for i in my_list[1::2]:
+    print(i , end= " ")
