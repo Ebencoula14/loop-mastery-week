3c69b75dda4feb9767f10ae51c9c3946e5ab5fa3
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-02 03:25:27 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,8 +1,5 @@
-prime_number = []
-for num in range(25,51):
-    for i in range(2, num):
-        if (num % i ) == 0:
-            break
-    else:
-        prime_number.append(num)
-print(prime_number)
+num = 1
+choice =int(input('Kindly pick any number of your choice\n'))
+for i in range(1,choice + 1 ):
+    num *= i
+print("The Factorial of the number you picked is : " , num)
