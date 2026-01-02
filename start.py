50841bd5500f5f46b70d9c25c9a78c0b615172dc
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-02 03:17:32 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
+prime_number = []
 for num in range(25,51):
     for i in range(2, num):
         if (num % i ) == 0:
             break
     else:
-        print(num)
+        prime_number.append(num)
+print(prime_number)
