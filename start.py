160731bd803904d085aa998ec088203852a99097
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-02 02:07:44 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,10 +1,3 @@
-from logging.config import stopListening
-
-number = [12, 75, 150, 180, 145, 525, 50]
-for i in number:
-    if i > 500:
-        break
-    elif i > 150:
-        continue
-    elif i % 5 == 0:
-        print(i)
+list1 = [10, 20, 30, 40, 50]
+for i in reversed(list1):
+    print(i)
