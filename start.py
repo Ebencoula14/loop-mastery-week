8b2bca59b918b1bf3fa7aa7a6c372a297eca1c09
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-08 02:24:51 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-score = 0
-while score < 10:
-    score += 1
-    print(score)
+number =int(input("Enter your preferable number\n"))
+j = 0
+for i in range(1, number + 1, 1):
+    j += i
+
+print("The sum of number you picked is : ", j)
