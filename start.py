97b18e0fcf73ff318f9017368a152067c7fe934c
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-08 19:24:09 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,8 +1,17 @@
-for num in range(25, 51):
+import random
+num = random.randint(1, 21)
+if num < 2:
+    is_prime = False
+else:
     is_prime = True
-    for i in range(2, int(num**0.5)+1):
-        if num % i == 0:
-            is_prime = False
-            break
-    if is_prime:
-        print(num)
+
+    for item in range(2, int(num**0.5)+ 1):
+
+        if num % item == 0:
+            is_prime= False
+        break
+
+    if is_prime :
+        print(num, "is a Prime number.")
+    else:
+        print(num,"is not a prime number.")
