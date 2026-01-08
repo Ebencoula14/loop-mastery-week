614d392860b4a6c2d37fd2022510c045062da99c
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-08 02:42:08 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
-number =int(input("Enter your preferable number\n"))
-j = 0
-for i in range(1,11):
-    print(f"{i * number}")
-
-
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for i in numbers:
+    if i > 500:
+        break
+    elif i > 150:
+        continue
+    elif i % 5 == 0:
+        print(i)
