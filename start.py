b10e501d24e9095d4d2394ab3d0f143286ebf634
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-08 04:48:14 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
-for i in range(5):
-    print(i)
-else:
-    print("Done!")
+for num in range(25, 51):
+    is_prime = True
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
