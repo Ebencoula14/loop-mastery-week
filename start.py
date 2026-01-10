460f2ee897b0a572d76ff6fe65a12e4071118b4c
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-10 04:43:09 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,3 +1,8 @@
 my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+odd_list = []
+count = 0
 for item in my_list[1::2]:
-    print(item, end=" ")
+    odd_list.append(item)
+for num in odd_list:
+    count += num
+print("The sum of ODD-INDEXED Numbers is : ",count)
