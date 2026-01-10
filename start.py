6c1a4841aa621ebfac2fda7af43962a7bfc66b07
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-10 04:29:41 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
 list_fibonacci = []
 a, b = 0 , 1
-for item in range(1, 10):
+for item in range(1, 23):
     a, b=b, a + b
     list_fibonacci.append(a)
-    if item > 100:
+    if b > 100:
         break
 print("The Fibonacci Series is: ", list_fibonacci)
