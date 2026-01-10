c0e288abbc9cc9826a3c11b87372a2ee49dfc5f1
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-10 18:24:18 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,8 +1,5 @@
-my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-odd_list = []
-count = 0
-for item in my_list[1::2]:
-    odd_list.append(item)
-for num in odd_list:
-    count += num
-print("The sum of ODD-INDEXED Numbers is : ",count)
+number = int(input("Kindly pick a RANDOM number?\n"))
+count = 1
+for item in range(1, number + 1):
+    count = count * item
+print(f"The Factorial of {number} is :", count)
