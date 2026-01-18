8d8173b551343f4173e7b5e0090e70871d5a0895
Ebencoula14 <Ebencoula14@gmail.com>
2026-01-19 00:37:35 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,5 +1,4 @@
-number = int(input("Kindly pick a RANDOM number?\n"))
-count = 1
-for item in range(1, number + 1):
-    count = count * item
-print(f"The Factorial of {number} is :", count)
+count = 0
+while count < 10:
+    count = count + 1
+    print(count)
