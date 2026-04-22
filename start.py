cd62726bdf7eac4a048db2a0ba0907852e73acd1
Ebencoula14 <Ebencoula14@gmail.com>
2026-04-22 15:43:30 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 numbers = [3, 7, 2, 9, 12, 15, 1]
 max_score = 0
+even_number = []
 for i in numbers:
-    if i > max_score:
-        max_score = i
-print(max_score)
+    if i % 2 == 0:
+        even_number.append(i)
+print("The number of the  EVEN numbers is : ", len(even_number))
