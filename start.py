8148d043e157147560c05310f4facb04931f0cf6
Ebencoula14 <Ebencoula14@gmail.com>
2026-04-23 15:26:09 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,6 +1,14 @@
+sum_of_odd = 0
+sum_of_even = 0
 for i in range(1, 101):
     if i % 2 == 0:
         number = "EVEN"
+        sum_of_even += i
     else:
         number = "ODD"
-    print(number, end= " ")
+        sum_of_odd += i
+#print(number, end= " ")
+print("The sum of ODD number inn the list is : " , sum_of_odd)
+print("The sum of EVEN number in the list is : " , sum_of_even)
+
+
