1afa98d2eeda391b2b0be815f19ba0fc5425e00d
Ebencoula14 <Ebencoula14@gmail.com>
2026-04-24 03:23:57 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,13 +1,4 @@
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 import random
-Heads = 0
-Tails = 0
-for i in range(10000):
-    toss = random.choice(['H', 'T'])
-    if toss == "H":
-        Heads += 1
-    else:
-        Tails += 1
-    percentage = ((Heads) / 10000) * 100
-print("The number of Heads is : " , Heads)
-print("the number of Tails is : ", Tails)
-print("The percentage of Heads is :" , percentage)
+choice = random.choice(friends)
+print(choice)
