e7628f92cf6dfa88bd15aaa9e4e9b853f78b48a4
Ebencoula14 <Ebencoula14@gmail.com>
2026-04-28 13:44:07 +0100

    introduction to loops 1

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
+from random import choice
+
 rock = '''
     _______
 ---'   ____)
@@ -24,51 +26,31 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-game_images = [rock, paper, scissors]
 import random
-name = input("HiHi, Welcome to the HOUSE PARTY, we are going to be having TONS OF FUN "
-             "TONIGHT,Y'll know the ROCK PAPER SCISSORS game right?\n").lower().title()
-if "yes" in name or "yaa" in name or "Of course" in name :
-    print("Good, let's play, I am excited!")
-    computer = random.randint(0,2)
-    while True:
-        user_name = input("What is the name?\n").lower()
-        if not user_name:
-            print("Error, you can't leave an empty space.")
-            break
-        elif "why" in user_name or "need" in user_name or "what" in user_name or "hmmm":
-            print("Well I need to know your name to proceed with the game")
+message = input("Hihi Everyone, we are all welcome to the HOUSE PARTY,"
+                             "we are going to be having a lot of FUN, we know the "
+                             "ROCK,PAPER SCISSORS game right??\n").lower()
+if "of course" in message or "yes" in message or "i think so" in message or "alright" in message or "yea" in message:
+    print("Sweet, let's play!")
+elif "no" in message or "nah"or "i haven't heard of it" in message or "what are the rules" or "what is that" in message:
+    print("Are you for real?")
+    choice = input("Are you guys interested? I can quickly bring you guys to speed?\n").lower()
+    if "yes" in choice or "cool" in choice or "of course" in choice:
+        print("ok, good these are the rules of the game.")
+        choice_rules_of_game=input("it is ROCK PAPER SCISSOR game, you are pick any of the three options"
+              "PAPER >> ROCK ,"
+              "ROCK >> SCISSORS and  SCISSORS >> PAPER do you get that?\n").lower()
+        if "yes" in choice_rules_of_game or " i get it " in choice_rules_of_game:
+            print("Sweet, them let's play!")
         else:
-            name = user_name.title()
-            print(f"oh {name}, it is nice to meet your acquaintance. let us play!")
-            break
-            user_choice = int(input(f"ok, {user_name},kindly pick between 0 (rock), 1 (paper), 2 (scissors)\n"))
-            if user_choice >= 0 and user_choice <= 2:
-                print(game_images[user_choice])
-            print(f"Computer chose:{game_images[computer]}")
-            if user_choice == computer:
-                print("It is a tie, let's try it again!")
-            elif user_choice == 0 and computer == 1:
-                print("The Computer wins!")
-            elif user_choice == 1 and computer == 2:
-                print("The computer wins!")
-            elif user_choice == 2 and computer == 1:
-                print(f"Hurray {name}, you won!")
-            elif user_choice == 2 and computer == 0:
-                print("The computer wins!")
-            elif user_choice >= 3 or user_choice < 0:
-                print(f"Hey {name}, you typed an invalid number, you lose!")
-            else:
-                print(f"Hurray {name}, you won!")
-else:
-    print("Really??")
-    alternative_choice = input("Why though??\n")
-while True:
-    alternative_name = input("I understand your feelings, they are VALID,but would you "
-                             "like to tell me your name?\n")
-    if alternative_name.strip() and alternative_name.lower() not in ["bad", "sad","not interested","weak"]:
-        name_variable = alternative_name.strip()
-        break
+            print("Go back to the rules of the game.")
     else:
-        print("I really don't mind knowing your name\n ")
-print("Alright",name_variable, "You should have a good one.")
+        name_of_uninterested=input("alright, no problem, i didn't get name tho'\n")
+    while True:
+        if not name_of_uninterested:
+            print("alright,You should have a GOOD one!")
+            break
+        elif "why" in name_of_uninterested or "what" in name_of_uninterested:
+            print("No problem, You should have good day!")
+            break
+    print("alright", name_of_uninterested,"enjoy the rest of your evening")
