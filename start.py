bbd9deaf991da6fb2b37de4ed84ab23f4b8e68ed
EBENCOULA <Ebencoula14@gmail.com>
2026-05-04 03:32:17 +0100

    password generator

--- start.py
+++ start.py
@@ -1,15 +1,9 @@
-Autumn = ["September", "October",  "November"]
-Winter = ["December", "January", "February" ]
-Summer = ["June", "July", "August"]
-Spring = ["March", "April", "May"]
-user_input = input("what MONTH is it over there?\n").title()
-if user_input in Autumn :
-    print("Oh It's AUTUMN already.")
-elif user_input in Winter:
-    print("WINTER is HERE!")
-elif user_input in Summer:
-    print("SUMMER BODY banging")
-elif user_input in Spring:
-    print("It's SPRING.")
+fruits = ['banana', 'orange', 'mango', 'lemon']
+user_input = input("What is you fave FRUIT?\n")
+if user_input not in fruits:
+    fruits.append(user_input)
+    print("Here is a MODIFIED list:", fruits)
+elif user_input in fruits:
+    print("We already have that in the LIST!")
 else:
-    print("??")
+    print("ERROR!")
