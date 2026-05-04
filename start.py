131bce93ebdaa08e5fe69ba449056019d7c32e17
EBENCOULA <Ebencoula14@gmail.com>
2026-05-04 19:30:33 +0100

    password generator

--- start.py
+++ start.py
@@ -1,9 +1,28 @@
-fruits = ['banana', 'orange', 'mango', 'lemon']
-user_input = input("What is you fave FRUIT?\n")
-if user_input not in fruits:
-    fruits.append(user_input)
-    print("Here is a MODIFIED list:", fruits)
-elif user_input in fruits:
-    print("We already have that in the LIST!")
-else:
-    print("ERROR!")
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+if "skills" in person:
+    middle_skill = len(person['skills'])// 2
+    print(person['skills'][middle_skill])
+    if "Python" in person["skills"]:
+        print(person['skills'])
+    if "JavaScript" and "React" in person["skills"]:
+        print("He is a FRONT END DEVELOPER!")
+    if "Node" and  "Python" and "MongoDB" in person["skills"]:
+        print("He is BACKEND DEVELOPER!")
+    if "React" and "Node" and "MongoDB":
+        print("He is a FULL STACK DEVELOPER!")
+    else:
+        print("UNKNOWN ERROR")
+    if person["is_married"] == True and person["country"] == "Finland":
+        print("Asabeneh Yetayeh lives in Finland. He is married.")
