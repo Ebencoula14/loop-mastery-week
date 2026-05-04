e4b3729ae41e0474e2a4d45afe2b81f640baf218
EBENCOULA <Ebencoula14@gmail.com>
2026-05-04 21:07:05 +0100

    function

--- start.py
+++ start.py
@@ -1,6 +1,10 @@
-def area_of_circle (r):
-    PI = 3.14
-    area = PI * r ** 2
-    return area
-user_input = int(input("what is the radius of your CIRCLE?\n"))
-print(area_of_circle(user_input))
+def add_two_numbers(num_1, num_2):
+    total = num_1 + num_2
+    return total
+user_input = input("Do you LIKE to the SUM of your two FAVORITE NUMBERS?\n")
+if "yes" in user_input or "of course" in user_input or "yea" in user_input:
+    first_number = int(input("So what is the first FAVORITE NUMBER?\n"))
+    second_number = int(input("the SECOND?\n"))
+    print("The SUM of your FAVORITE NUMBERS is:",add_two_numbers(first_number, second_number))
+else:
+    print("Well, it SEEMS you are NOT INTERESTED")
