c6a4d68157861b9a04ad43263aaccd82657ad92a
EBENCOULA <Ebencoula14@gmail.com>
2026-05-04 20:38:07 +0100

    function

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
-def sum_of_numbers(n):
-    total = 0
-    for i in range(n+1):
-        total+=i
-    return total
-user_input = int(input("what is your FAVORITE numbers, and I will tell you THE SUM!\n"))
-print(sum_of_numbers(user_input))
+def area_of_circle (r):
+    PI = 3.14
+    area = PI * r ** 2
+    return area
+user_input = int(input("what is the radius of your CIRCLE?\n"))
+print(area_of_circle(user_input))
