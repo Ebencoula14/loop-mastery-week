c8b3a3c80e3416ef0946816fa191cb8a59cdf2c7
EBENCOULA <Ebencoula14@gmail.com>
2026-05-04 03:00:45 +0100

    password generator

--- start.py
+++ start.py
@@ -1,10 +1,13 @@
-my_age = 30
-your_age = int(input("How OLD are YOU?\n"))
-diff = my_age - your_age
-diff_1 = your_age - my_age
-if my_age > your_age:
-    print(f"I am {diff} years older than YOU!")
-elif your_age > my_age:
-    print(f"you are {diff_1} years older than ME! ")
+score = int(input("Kindly ENTER your SCORES here!\n"))
+if score >= 0 and score <= 59:
+    print(f"YOU FAILED THE COURSE,{score}F")
+elif 60 <= score <= 69:
+    print(f"YOU ESCAPED,{score}D")
+elif 70 <= score <= 79:
+    print(f"FAIR,{score}C")
+elif 80 <= score <= 89:
+    print(f"EXCELLENT,{score}B")
+elif 90 <= score <= 100:
+    print(f"SCHOLAR! {score}A")
 else:
-    print(f"We are AGE MATES!")
+    print("DON'T WASTE MY TIME!")
