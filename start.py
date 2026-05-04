32a1ec5e46631135c6f7435c32d409d8a7e79882
EBENCOULA <Ebencoula14@gmail.com>
2026-05-04 03:23:43 +0100

    password generator

--- start.py
+++ start.py
@@ -1,13 +1,15 @@
-score = int(input("Kindly ENTER your SCORES here!\n"))
-if score >= 0 and score <= 59:
-    print(f"YOU FAILED THE COURSE,{score}F")
-elif 60 <= score <= 69:
-    print(f"YOU ESCAPED,{score}D")
-elif 70 <= score <= 79:
-    print(f"FAIR,{score}C")
-elif 80 <= score <= 89:
-    print(f"EXCELLENT,{score}B")
-elif 90 <= score <= 100:
-    print(f"SCHOLAR! {score}A")
+Autumn = ["September", "October",  "November"]
+Winter = ["December", "January", "February" ]
+Summer = ["June", "July", "August"]
+Spring = ["March", "April", "May"]
+user_input = input("what MONTH is it over there?\n").title()
+if user_input in Autumn :
+    print("Oh It's AUTUMN already.")
+elif user_input in Winter:
+    print("WINTER is HERE!")
+elif user_input in Summer:
+    print("SUMMER BODY banging")
+elif user_input in Spring:
+    print("It's SPRING.")
 else:
-    print("DON'T WASTE MY TIME!")
+    print("??")
