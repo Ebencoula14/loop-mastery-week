4bb7f8a340ab6a11c76c8979a4692a7026860f3d
EBENCOULA <Ebencoula14@gmail.com>
2026-05-17 00:52:06 +0100

    function

--- start.py
+++ start.py
@@ -1,6 +1,4 @@
-def greetings(first_name, last_name):
-    space = " "
-    full_name = first_name + space + last_name
-    message = full_name + " " + "Welcome to The Fun House Party"
-    return message
-print(greetings("OLUWATOBI", "OLADIMEJI"))
+def weight_of_object(mass, gravity):
+    weight = str(mass * gravity) + "N"
+    return weight
+print(weight_of_object(100, 9.81))
