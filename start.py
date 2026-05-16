f93a0acb5d162bf26b73d1355b6d892fbb37eb5f
EBENCOULA <Ebencoula14@gmail.com>
2026-05-17 00:47:48 +0100

    function

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
-def area_of_a_circle(r):
-    PIE = 3.142
-    area = PIE * r * r
-    return area
-user_input = int(input("what is the RADIUS of the CIRCLE?\n"))
-print(area_of_a_circle(user_input))
+def greetings(first_name, last_name):
+    space = " "
+    full_name = first_name + space + last_name
+    message = full_name + " " + "Welcome to The Fun House Party"
+    return message
+print(greetings("OLUWATOBI", "OLADIMEJI"))
