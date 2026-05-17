0cbe49403e21728ac53a586f69ddd6efe5d78af9
EBENCOULA <Ebencoula14@gmail.com>
2026-05-17 13:19:54 +0100

    function

--- start.py
+++ start.py
@@ -1,3 +1,6 @@
-def greetings(name, location):
-    print("Hi There", name, "How is the weather at", location)
-greetings("Tobi", "Lagos")
+def add_two_numbers(num_1, num_2):
+    total = num_1 + num_2
+    return total
+num_1 = int(input("kindly choose any arbitrary number?\n"))
+num_2 = int(input("kindly pick another arbitrary number please?\n"))
+print("The sum of your numbers is :", add_two_numbers(num_1,num_2))
