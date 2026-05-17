010b949414bd9bceeba0f8e8fab3e65929b99d98
EBENCOULA <Ebencoula14@gmail.com>
2026-05-17 14:04:57 +0100

    function

--- start.py
+++ start.py
@@ -1,5 +1,15 @@
-def convert_celsius_to_fahrenheit(C):
-    F = (C * 9 / 5) + 32
-    return  F
-C = int(input("What is the degree in celsius?\n"))
-print(convert_celsius_to_fahrenheit(C))
+def check_season(month):
+    season = " "
+    if month == "December" or month == "January" or month == "February":
+        season = "WINTER"
+    elif month == "March" or month == "April" or month == "May":
+        season = "SPRING"
+    elif month == "June" or month == "July" or month == "August":
+        season = "SUMMER"
+    elif month =="September" or month == "October" or month == "November":
+        season ="AUTUMN"
+    else:
+        season = "INVALID"
+    return season
+month = input("What month is it over there?\n").title()
+print("Oh it is ", check_season(month))
