939dd7210e5587b5564f5b880cdac85e42ec9054
EBENCOULA <Ebencoula14@gmail.com>
2026-05-17 12:43:51 +0100

    function

--- start.py
+++ start.py
@@ -1,5 +1,12 @@
-def number_is_even(n):
-    if n % 2 == 0:
-        return True
-    return False
-print(number_is_even(4564))
+
+def even_numbers(n):
+    even =[]
+    odd = []
+    for i in range(n+1):
+        if i % 2 ==0:
+            even.append(i)
+    return even
+
+
+n =int(input("pick any arbitrary number of your choice and  i will list the even NUMBERS iin that range\n "))
+print(even_numbers(n))
