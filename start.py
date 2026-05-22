d9a0de1843b4171a1ceb84975e6d4b300b4c046d
EBENCOULA <Ebencoula14@gmail.com>
2026-05-22 14:36:54 +0100

    listing the elements of a list in capitalized format

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
 def generate_fruit():
     fruits = ["orange", "banana", "apple","grape", "breadfruit", "lemon"]
+    new_fruits = []
     for item in fruits:
         capitalized_item = item.upper()
-        print(capitalized_item)
+        new_fruits.append(capitalized_item)
+    print(new_fruits)
 
 generate_fruit()
