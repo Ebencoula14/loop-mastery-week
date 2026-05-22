e6071b8fe98a4ba9effbc4aa51e746a4b7e457bb
EBENCOULA <Ebencoula14@gmail.com>
2026-05-22 15:08:07 +0100

    updating a list of elements with a new user choice

--- start.py
+++ start.py
@@ -1,9 +1,6 @@
-def generate_fruit():
-    fruits = ["orange", "banana", "apple","grape", "breadfruit", "lemon"]
-    new_fruits = []
-    for item in fruits:
-        capitalized_item = item.upper()
-        new_fruits.append(capitalized_item)
-    print(new_fruits)
-
-generate_fruit()
+def add_item(n):
+    food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk']
+    food_stuff.append(n)
+    return  food_stuff
+n = input("What is your favorite FOODSTUFF?\n")
+print("Ok the updated LIST is : ", add_item(n))
