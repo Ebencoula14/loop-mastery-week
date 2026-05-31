f441f9fe74ff9eca7d9c15a7520340c50dde67e9
EBENCOULA <Ebencoula14@gmail.com>
2026-05-31 14:10:05 +0100

    iterating a loop using he while function

--- start.py
+++ start.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2,int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-user_choice = int(input("what is your favorite number?\n"))
-print("Prime Number?", is_prime(user_choice))
+count = 0
+while count < 10:
+    print(count)
+    count = count + 1
+else:
+    print(count)
