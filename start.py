6611d1d8a61f7055fbf7fa819ab828cac47053eb
EBENCOULA <Ebencoula14@gmail.com>
2026-05-31 19:58:58 +0100

    iterating a loop using the while function

--- start.py
+++ start.py
@@ -1,8 +1,5 @@
-count = 0
-score = 0
-for item in range(101):
-    if item % 2 == 0 and item != 0:
-        count += item
-    else:
-        score += item
-print("The sum of EVEN numbers is ", count,"."" And the sum of ODD numbers is", score,"." )
+fruit_list = ['banana', 'orange', 'mango', 'lemon']
+new_list = []
+for item in reversed(fruit_list):
+    new_list.append(item)
+print(new_list)
