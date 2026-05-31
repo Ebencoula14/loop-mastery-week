dc2305243900d95ae4ba88cca32268106bb005d9
EBENCOULA <Ebencoula14@gmail.com>
2026-05-31 19:31:19 +0100

    iterating a loop using the while function

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
 count = 0
+score = 0
 for item in range(101):
-    count += item
-print(count)
+    if item % 2 == 0 and item != 0:
+        count += item
+    else:
+        score += item
+print("The sum of EVEN numbers is ", count,"."" And the sum of ODD numbers is", score,"." )
